0db9f72982b665937a62d6ce7b2b25336e4219ef
ezequiel <ezequiel@ZAS-LAPTOP>
2024-08-22 13:19:55 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoTest/Controllers/HomeController.cs
@@ -0,0 +1,132 @@
+﻿using ProyectoTest.Models;
+using ProyectoTest.Logica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace ProyectoTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Session["Usuario"] == null)
+                return RedirectToAction("Index", "Login");
+
+            return View();
+        }
+
+        public ActionResult Producto()
+        {
+            if (Session["Usuario"] == null)
+                return RedirectToAction("Index", "Login");
+
+            return View();
+        }
+
+        public ActionResult Tienda()
+        {
+            if (Session["Usuario"] == null)
+                return RedirectToAction("Index", "Login");
+
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public JsonResult ListarProducto()
+        {
+            List<Producto> oLista = new List<Producto>();
+
+            oLista = ProductoLogica.Instancia.Listar();
+            oLista = (from o in oLista
+                      select new Producto()
+                      {
+                          IdProducto = o.IdProducto,
+                          Nombre = o.Nombre,
+                          Descripcion = o.Descripcion,
+                          Precio = o.Precio,
+                          Stock = o.Stock,
+                          RutaImagen = o.RutaImagen,
+                          base64 = utilidades.convertirBase64(Server.MapPath(o.RutaImagen)),
+                          extension = Path.GetExtension(o.RutaImagen).Replace(".", ""),
+                          Activo = o.Activo
+                      }).ToList();
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GuardarProducto(string objeto, HttpPostedFileBase imagenArchivo)
+        {
+
+            Response oresponse = new Response() { resultado = true, mensaje = "" };
+
+            try
+            {
+                Producto oProducto = new Producto();
+                oProducto = JsonConvert.DeserializeObject<Producto>(objeto);
+
+                string GuardarEnRuta = "~/Imagenes/Productos/";
+                string physicalPath = Server.MapPath("~/Imagenes/Productos");
+
+                if (!Directory.Exists(physicalPath))
+                    Directory.CreateDirectory(physicalPath);
+
+                if (oProducto.IdProducto == 0)
+                {
+                    int id = ProductoLogica.Instancia.Registrar(oProducto);
+                    oProducto.IdProducto = id;
+                    oresponse.resultado = oProducto.IdProducto == 0 ? false : true;
+
+                }
+                else
+                {
+
+                    oresponse.resultado = ProductoLogica.Instancia.Modificar(oProducto);
+
+                }
+
+
+                if (imagenArchivo != null && oProducto.IdProducto != 0)
+                {
+                    string extension = Path.GetExtension(imagenArchivo.FileName);
+                    GuardarEnRuta = GuardarEnRuta + oProducto.IdProducto.ToString() + extension;
+                    oProducto.RutaImagen = GuardarEnRuta;
+
+                    imagenArchivo.SaveAs(physicalPath + "/" + oProducto.IdProducto.ToString() + extension);
+
+                    oresponse.resultado = ProductoLogica.Instancia.ActualizarRutaImagen(oProducto);
+                }
+
+            }
+            catch (Exception e)
+            {
+                oresponse.resultado = false;
+                oresponse.mensaje = e.Message;
+            }
+
+            return Json(oresponse, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult EliminarProducto(int id)
+        {
+            bool respuesta = false;
+            respuesta = ProductoLogica.Instancia.Eliminar(id);
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class Response
+    {
+
+        public bool resultado { get; set; }
+        public string mensaje { get; set; }
+    }
+
+}
---
+++ ProyectoTest/Logica/Conexion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTest.Logica
+{
+    public class Conexion
+    {
+        public static string CN = @"Data Source=SQL8010.site4now.net;Initial Catalog=db_aabb9c_bdtienda;User Id=db_aabb9c_bdtienda_admin;Password=zero1202";
+        //public static string CN = @"Data Source=ZAS-LAPTOP\SQLEXPRESS;Database=bdtienda;Integrated Security=True;TrustServerCertificate=true";
+        //"Data Source=SQL8010.site4now.net;Initial Catalog=db_aabb9c_bdtienda;User Id=db_aabb9c_bdtienda_admin;Password=YOUR_DB_PASSWORD
+
+
+    }
+}
