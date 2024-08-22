ac11edc405b250eed6f0aa2d78e57548122a01a2
w-ezequiel <57279797+w-ezequiel@users.noreply.github.com>
2024-08-22 13:29:39 -0600

    Update Conexion.cs

--- ProyectoTest/Logica/Conexion.cs
+++ ProyectoTest/Logica/Conexion.cs
@@ -8,8 +8,7 @@ namespace ProyectoTest.Logica
     public class Conexion
     {
         public static string CN = @"Data Source=SQL8010.site4now.net;Initial Catalog=db_aabb9c_bdtienda;User Id=db_aabb9c_bdtienda_admin;Password=zero1202";
-        //public static string CN = @"Data Source=ZAS-LAPTOP\SQLEXPRESS;Database=bdtienda;Integrated Security=True;TrustServerCertificate=true";
-        //"Data Source=SQL8010.site4now.net;Initial Catalog=db_aabb9c_bdtienda;User Id=db_aabb9c_bdtienda_admin;Password=YOUR_DB_PASSWORD
+      
 
 
     }
