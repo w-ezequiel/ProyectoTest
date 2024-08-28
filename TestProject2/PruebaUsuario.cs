ea4d34bf5ca0c0a3b1e99091f2d43eaced44a3ca
w-ezequiel <oa1283032018@unab.edu.sv>
2024-08-27 18:36:04 -0600

    prueba Nunit final

---
+++ TestProject2/PruebaUsuario.cs
@@ -0,0 +1,46 @@
+﻿using ProyectoTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Prueba_Nunit
+{
+    internal class PruebaUsuario:Controller
+    {
+
+        public ActionResult Producto()
+        {
+            Usuario user = new Usuario();
+            user.IdUsuario = 1;
+            user.Correo = "usuario@gmail.com";
+            user.Contra = "1234";
+
+            Session["Usuario"] = user;
+
+            if (Session["Usuario"] == null)
+                return View("Login");
+
+            return View();
+        }
+
+        public ActionResult validar(Usuario user2)
+        {
+           
+            if (user2.EsAdministrador == true)
+            {
+                return View("Home");
+            }
+            else
+            {
+                return View("Tienda");
+            }
+        }
+       
+
+    }
+}
+
+
--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using ProyectoTest.Controllers;
 using ProyectoTest.Logica;
 using ProyectoTest.Models;
+using Prueba_Nunit;
 using System.Web.Mvc;
 
 using System.Web.Security;
@@ -29,16 +30,33 @@ namespace TestProject2
             Assert.That(result,Is.Not.Null);
         }
 
-
         [Test]
         public void Test2()
         {
-           
-            var prueba = new PruebaProductoController();
+            Usuario user2 = new Usuario();
+            user2.IdUsuario = 1;
+            user2.Correo = "usuario@gmail.com";
+            user2.Contra = "1234";
+            user2.EsAdministrador = false;
 
-            var result = prueba.EliminarProducto(1) as ActionResult;
+            var login = new PruebaUsuario();
 
-            Assert.That(result, Is.Not.Null);
+            var result = login.validar(user2) as ViewResult;
+
+            Assert.AreEqual("Tienda", result.ViewName);
         }
+
+
+        //[Test]
+        //public void Test2()
+        //{
+
+        //    var prueba = new PruebaProductoController();
+
+        //    var result = prueba.EliminarProducto(1) as ViewResult;
+        //    //var result = prueba.EliminarProducto(4) ;
+
+        //    Assert.AreEqual("Index", result.ViewName);
+        //}
     }
 }
